c7b384e296c34a7350c11c7563ea72d46a4c9b74
Ghallabi <ghallabi.farouk@gmail.com>
2025-09-22 12:53:55 +0100

    clean init

--- src/dataup_models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .geom import BoundingBox, Polygon
-from .labels import Label
-from .ml import DetectorParams, SAMParams
-
-
-__all__ = ["BoundingBox","Polygon","Label", "DetectorParams", "SAMParams"]
