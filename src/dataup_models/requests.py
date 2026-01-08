fc51d565b29732caf4c4aec3a46a6dbf00d17014
Ghallabi <ghallabi.farouk@gmail.com>
2026-01-08 14:53:04 +0000

    fix InferenceRequest issue

--- src/dataup_models/requests.py
+++ src/dataup_models/requests.py
@@ -8,7 +8,7 @@ ModelParams = Annotated[DetectorParams | SAM3Params, Field(discriminator="param_
 class InferenceRequest(BaseModel, frozen=True):
     request_id: str | None = Field(description="Unique request ID", default=None)
     image_urls: list[str] | None = Field(description="List of image URLs to run inference on", default=None)
-    images_b64: list[HttpUrl] | None = Field(description="List of base64 encoded images to run inference on", default=None)
+    images_b64: list[str] | None = Field(description="List of base64 encoded images to run inference on", default=None)
     session_id: str | None = Field(description="Session ID", default=None)
     params: ModelParams = Field(default_factory=DetectorParams, description="Model inference parameters")
 
